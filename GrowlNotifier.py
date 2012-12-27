fffb324cb4572a03ba8388c4ff54c7cb22f43cc1
Corbinian Bergunde <admin@corbinian-bergunde.de>
2012-12-27 05:31:59 +0100

    GrowlNotifier 0.0.1

---
+++ GrowlNotifier.py
@@ -0,0 +1,121 @@
+__version__ = "0.0.1"
+__author__ = "Corbinian Bergunde (http://corbinian-bergunde.de)"
+__copyright__ = "(C) 2012 Corbinian Bergunde. Code under BSD License."
+
+import sublime,sublime_plugin,traceback,sys
+
+
+class GrowlNotifier:
+
+    def __init__(self):
+        events()
+
+    def sendmessage(self,message):
+        import platform
+        import os
+
+        Mac = "Darwin"
+        Windows = "Windows"
+        Linux = "Linux"
+
+        if platform.system() == Mac:
+            #Snow Leopard detected
+            if platform.release() == "10.8.0":
+                self.netgrowlnotification(message)
+            #Lion 10.7+  detected
+            elif platform.release() >= "10.8.0":
+                self.gntpnotification(message)
+            #below snow leopard  deteced
+            else:
+                self.netgrowlnotification(message)
+
+
+        elif platform.system() == Windows:
+                self.gntpnotification(message)
+
+        elif platform.system() == Linux:
+                self.gntpnotification(message)
+
+        pass
+
+    def netgrowlnotification(self,message):
+        """
+        Growl Version:Growl  1.2
+        Protocol:UDP
+        Note:Growl version 1.2 only works with the old protocol UDP.
+        """
+        try:
+            import netgrowl
+            import socket
+
+            PASSWORD = ''
+            #create Socket
+            addr = ("localhost", netgrowl.GROWL_UDP_PORT)
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            #register Growl packet
+            p = netgrowl.GrowlRegistrationPacket(application='Sublime Text 2', password=PASSWORD)
+            p.addNotification()
+            sock.sendto(p.payload(), addr)
+            #send message
+            p = netgrowl.GrowlNotificationPacket(password=PASSWORD, application='Sublime Text 2',
+            title="Sublime Text 2", description=message,priority=1,sticky=False)
+            sock.sendto(p.payload(), addr)
+            #close Socket
+            sock.close()
+        except Exception as e:
+            raise e
+
+    def gntpnotification(self,message):
+          """
+          Growl Version:Growl  1.3
+          Protocol:GNTP
+          Note:Growl Version 1.3 works with the new protocol  gntp.
+          """
+          import gntp.notifier
+
+          try:
+
+            growl = gntp.notifier.GrowlNotifier(
+                applicationName = "Sublime Text 2",
+                notifications = [title,message],
+                )
+            growl.register()
+
+            growl.notify(
+                noteType = "New Messages",
+                title = "Sublime Text 2",
+                description = message,
+                icon = "http://d2o0t5hpnwv4c1.cloudfront.net/1038_sublime/sublime.png",
+                sticky = False,
+                priority = 1,)
+
+          except Exception as e:
+              print e
+
+
+class events(sublime_plugin.EventListener):
+
+
+
+    def on_new(self,view):
+        GrowlNotifier().sendmessage("Open new Window")
+
+    def on_clone(self,view):
+        GrowlNotifier().sendmessage("View was Cloned")
+
+    def on_load(self,view):
+        print view.file_name()
+        GrowlNotifier().sendmessage('File: "' + view.file_name() + '" were loaded')
+
+    def on_close(self,view):
+        GrowlNotifier().sendmessage("Window were closed")
+
+    def on_post_save(self,view):
+        GrowlNotifier().sendmessage('File: "' + view.file_name() +  '" were saved')
+
+
+
+
+
+
+events()
---
+++ readme.md
@@ -0,0 +1,63 @@
+###GrowlNotifier for Sublime Text 2
+
+#About
+GrowlNotifier is a plugin which use growl notfication with user events.
+At this moments the plugin support a few events but i promise it will support more.
+
+
+###Important
+If you use Snow Leopard or Growl version 1.2 you have to allow "Incoming Messages" in the growl preferences.
+
+
+## Sublime Text 2 Installation
+
+#Package Control
+It's recommend to use the the Sublime 'Package Control'(http://wbond.net/sublime_packages/package_control) to install this and other packages.
+
+If you've installed 'Package Control' press 'cmd' + 'shift' + 'p' on Mac  on Windows 'ctrl' + 'shift' + 'p'.
+And insert : "Package Control:Install Package" and press "Enter".
+Search for 'GrowlNotifier' and press Enter.
+Now you've installed succesfully GrowlNotfifier.
+
+###Credits
+The Package use two libraries:
+
+##Netgrowl
+Author:Rui Carmo <http://the.taoofmac.com>
+
+copyright:(C) 2004 Rui Carmo. Code under BSD License.
+
+link: http://the.taoofmac.com/space/projects/netgrowl 
+
+##gntp
+Author:Paul Traylor <http://kungfudiscomonkey.net>
+
+link: https://github.com/kfdm/gntp
+
+Copyright:
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+##License:
+Copyright:2012 (c) Corbinian Bergunde <http://corbinian-bergunde.de>
+
+License:BSD
+
